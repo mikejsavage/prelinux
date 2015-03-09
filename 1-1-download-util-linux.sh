6766e5ec0a2a148b092812346d11eb79a2aa1257
Michael Savage <mikejsavage@gmail.com>
2015-03-09 15:06:22 +0000

    Bump util-linux version

--- 1-1-download-util-linux.sh
+++ 1-1-download-util-linux.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-version=2.25.2
+version=2.26
 dir=util-linux-$version
 archive=$dir.tar.gz
 
-curl https://kernel.org/pub/linux/utils/util-linux/v${version%.*}/$archive -o $archive
+curl https://kernel.org/pub/linux/utils/util-linux/v$version/$archive -o $archive
 tar xf $archive
 mv $dir util-linux
